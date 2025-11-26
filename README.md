83c7bc42b00c4ec85e0cbc0781efeaa3c187855d
Javier Perez <337745+dvst@users.noreply.github.com>
2025-11-26 14:49:12 -0500

    DVC configured with data ingestion and preprocessing scripts

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# youtube-sentiment-analysis
+conda create -n youtube python=3.11 -y
+
+conda activate youtube
+
+pip install -r requirements.txt
+
+
+## DVC
+
+dvc init
+
+dvc repro
+
+dvc dag
+
+## AWS
+
+aws configure
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+	name = 'youtube',
+	version='0.0.0',
+	author='Bappy-with-javitech',
+	author_email='noneedfora@email.com',
+	packages= find_packages(),
+	install_requires=[]
+)
